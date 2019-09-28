0bd07f5c5def6800dedbe7518f609500b232793f
Hsin-Mao Wu <hsinmao@maoshouse.com>
2019-09-28 01:20:13 -0700

    Remove unused credentials classes.

--- src/main/java/com/maoshouse/blonk/credentials/BlonkCredentials.java
+++
@@ -1,8 +0,0 @@
-package com.maoshouse.blonk.credentials;
-
-import com.maoshouse.blonk.exception.CredentialsException;
-import com.maoshouse.blonk.model.AuthToken;
-
-public interface BlonkCredentials {
-    AuthToken getAuthToken() throws CredentialsException;
-}
